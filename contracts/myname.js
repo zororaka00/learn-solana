2aefac44081c12cb11072720e3febf7ecd7c5b71
tcraka <raka.antoro@tokocrypto.com>
2022-08-01 05:51:56 +0700

    .

--- contracts/myname.js
+++ contracts/myname.js
@@ -14,12 +14,14 @@ const BUNDLE_SO = readFileSync('./build/bundle.so')
         'confirmed'
     );
 
-    const payer = Keypair.generate();
+    // const payer = Keypair.generate();
+    const payer = Keypair.fromSecretKey(Uint8Array.from([ 230, 30, 115, 150, 107, 15, 219, 101, 241, 134, 39, 71, 112, 173, 148, 85, 72, 113, 195, 234, 65, 212, 91, 155, 248, 220, 136, 109, 251, 239, 210, 33, 232, 16, 201, 27, 137, 70, 146, 162, 39, 245, 161, 162, 246, 212, 179, 140, 227, 71, 162, 148, 200, 168, 56, 186, 170, 209, 34, 133, 102, 74, 6, 75 ]));
     console.log('Airdropping (from faucet) SOL to a new wallet ...');
     const signature = await connection.requestAirdrop(payer.publicKey, LAMPORTS_PER_SOL);
     await connection.confirmTransaction(signature, 'confirmed');
-    console.log('balance: ', await connection.getBalance(payer.publicKey));
-    const program = Keypair.generate();
+    console.log('balance: ', await connection.getBalance(payer.publicKey) / LAMPORTS_PER_SOL);
+    // const program = Keypair.generate();
+    const program = Keypair.fromSeed(Uint8Array.from([70,60,102,100,70,60,102,100,70,60,102,100,70,60,102,100,70,60,102,100,70,60,102,100,70,60,102,100,70,60,102,100]));
     const storage = Keypair.generate();
     console.log('payer: ', payer.publicKey.toBase58());
     console.log('program: ', program.publicKey.toBase58());
@@ -31,7 +33,7 @@ const BUNDLE_SO = readFileSync('./build/bundle.so')
     await contract.load(program, BUNDLE_SO);
 
     console.log('Program deployment finished, deploying myname ...');
-    await contract.deploy('MyName', [], storage, 4096 * 8);
+    await contract.deploy('MyName', [], storage, 2000);
 
     console.log('Contract deployment finished, invoking contract functions ...');
     const version = await contract.version();
@@ -48,6 +50,8 @@ const BUNDLE_SO = readFileSync('./build/bundle.so')
     const address = publicKeyToHex(payer.publicKey);
     const getname = await contract.myname(address);
     console.log(`name for address ${address} is ${getname}`);
+
+    console.log('balance: ', await connection.getBalance(payer.publicKey) / LAMPORTS_PER_SOL);
 
     process.exit(0)
 })()
